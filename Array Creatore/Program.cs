c3e35664db33b42c798b63b3aadc892f3ca36bca
Igor <tararuev65@gmail.com>
2023-03-26 14:29:40 +0600

    Array Creatore/

---
+++ Array Creatore/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int[] ArrayCreatore(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array [i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return array;
+} 
+
+int[] array = ArrayCreatore(5, 1, 9);
+Console.WriteLine (String.Join(", ", array));
