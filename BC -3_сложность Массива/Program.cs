89b4505641f71e8859bfed577fc447c07d5cf90e
Igor <tararuev65@gmail.com>
2023-03-27 15:48:46 +0600

    BC-сложность

---
+++ BC -3_сложность Массива/Program.cs
@@ -0,0 +1,11 @@
+﻿// создаем массив - заполняее его пользователь- на 5 эл
+        
+Console.Clear();
+Console.WriteLine("Введите числа: ");
+
+int[] array = new int[5];
+    for (int i = 0; i < 5; i++)
+     array[i] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("[" + String.Join(", ", array) + "]");
+    
+
